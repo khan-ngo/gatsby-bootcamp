2f319a11dcead01aaa73c072a45ab5510b61190e
Khanh Ngo <55854113+khanh-ngo-github@users.noreply.github.com>
2019-11-06 07:23:21 -0500

    add style to make footer sticky

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,12 @@ import layoutStyles from './layout.module.scss'
 const Layout = (props) => {
     return (
         <div className={layoutStyles.container}>
+
+            <div className={layoutStyles.content}>
             <Header />
+
             {props.children}
+            </div>
             <Footer />
         </div>
     )
