13454b1aaa132393adc99602e7d84b0330029034
Khanh Ngo <55854113+khanh-ngo-github@users.noreply.github.com>
2019-11-05 23:26:29 -0500

    initial commit

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>This contains infomration for how to contact us.</p>
+        </div>
+    )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h2> I'm Khanh, a full-stack developer living in Beautiful Boston.</h2>
+            <p>Need a developer? <Link to="/contact">Contact Me</Link></p>
+
+        </div>
+    )
+}
+
+export default IndexPage
