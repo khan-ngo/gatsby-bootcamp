799df007d2efcaf4128d352d4dd3fe4da8954cb1
Khanh Ngo <55854113+khanh-ngo-github@users.noreply.github.com>
2019-11-06 01:10:30 -0500

    add module scss

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import headerStyles from './header.module.scss'
 
 const Header = () => {
     return (
@@ -8,7 +9,7 @@ const Header = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link className={headerStyles.link} to="/">Home</Link>
                     </li>
                     <li>
                         <Link to="/about">About</Link>
